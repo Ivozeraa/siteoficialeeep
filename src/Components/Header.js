4a53102327d1604a86f1b255314c7fc58a263df4
Ivozeraa <joseivo2008@gmail.com>
2024-10-03 09:27:29 -0300

    Mudanças

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FaUserCircle } from 'react-icons/fa'; // Ícone para o perfil
 import '../Css/Header.css';
 
 const Header = () => {
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
+  const handleLogout = () => {
+    // Função para deslogar o usuário
+    console.log('Deslogar');
+  };
+
   return (
     <header className="main-header">
+      <div className="logo-container">
+        <img src="https://eeep-irma-ana-zelia.netlify.app/img/logo-da-empresa.png" alt="Logo" className="logo" />
+        <h1 className="school-name">EEEP</h1>
+      </div>
+
       <nav className="navbar">
         <ul>
           <li><Link to="/">Home</Link></li>
@@ -18,9 +35,16 @@ const Header = () => {
               <Link to="/contato">Contato</Link>
             </div>
           </li>
+          <li className="user-settings">
+            <FaUserCircle size={40} className="user-icon" onClick={toggleDropdown} />
+            {dropdownVisible && (
+              <ul className="settings-dropdown">
+                <li><Link to="/configuracoes">Configurações</Link></li>
+                <li><button onClick={handleLogout}>Deslogar</button></li>
+              </ul>
+            )}
+          </li>
         </ul>
-        <div className="user-settings">
-        </div>
       </nav>
     </header>
   );
